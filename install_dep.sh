999cf72092f0cef7d874389bd1fe76e3273ba2d0
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-02-21 17:10:35 +0100

    install more modern meson

--- install_dep.sh
+++ install_dep.sh
@@ -2,7 +2,9 @@
 
 # Install all the dependencies needed to build QOpenHD from source.
 
-apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good
-apt -y install git meson
+apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good ruby
+apt -y install git
+
+pip3 install meson
 
 gem install fpm
