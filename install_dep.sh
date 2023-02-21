2eb30491d8fba6a822d332f8153a1ded89c14013
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-02-21 17:14:40 +0100

    meson path

--- install_dep.sh
+++ install_dep.sh
@@ -6,5 +6,8 @@ apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good ruby
 apt -y install git
 
 pip3 install meson
+#dirty fix for meson not being found when not in /usr/bin
+cp /usr/local/bin/meson /usr/bin/meson
+
 
 gem install fpm
