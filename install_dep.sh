8d0eec9badf2d425ff5886e0562013d838ed4a50
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-02-21 17:53:18 +0100

    Update install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -2,8 +2,8 @@
 
 # Install all the dependencies needed to build QOpenHD from source.
 
-apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good ruby
-apt -y install git flex bison
+apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good python3 ruby
+apt -y install git flex bison 
 
 pip3 install meson
 #dirty fix for meson not being found when not in /usr/bin
