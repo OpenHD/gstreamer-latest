a68999c7e7a32cee74e2ba9496bdb9d33532ffe5
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-02-22 10:55:12 +0100

    add cmake

--- install_dep.sh
+++ install_dep.sh
@@ -3,7 +3,7 @@
 # Install all the dependencies needed to build QOpenHD from source.
 
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good python3-pip ruby ninja-build
-apt -y install git flex bison tree qtbase5-dev-tools
+apt -y install git flex bison tree qtbase5-dev-tools cmake
 
 pip3 install meson
 #dirty fix for meson not being found when not in /usr/bin
