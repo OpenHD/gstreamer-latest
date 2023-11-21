2275e71444e7952b2fbe4892de68620a2d707260
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-11-21 05:59:17 +0100

    debug upload

--- build_chroot.sh
+++ build_chroot.sh
@@ -6,14 +6,15 @@ sudo apt remove meson
 sudo apt install -y python3-pip libdrm-dev
 sudo pip3 install --upgrade cloudsmith-cli
 sudo pip3 install meson
-bash install_build_dep.sh
+#bash install_build_dep.sh
 meson setup --prefix=/tmp/gst-plugins-good/usr -Dgood=enabled -Dgst-plugins-good:qt5=enabled build 
 cd build
-ninja
-ninja install
-fpm -a armhf -s dir -t deb -n gst-latest -v 2.5-evo-$(date '+%m%d%H%M') -C /tmp/gst-plugins-good -p gst-latest-1.22.deb
+#ninja
+#ninja install
+#fpm -a armhf -s dir -t deb -n gst-latest -v 2.5-evo-$(date '+%m%d%H%M') -C /tmp/gst-plugins-good -p gst-latest-1.22.deb
+touch gst-latest-1.22.deb
 mkdir -p /opt/out/
-cp -v *.dep /opt/out/
+cp -v *.deb /opt/out/
 echo "copied deb file"
 echo "push to cloudsmith"
 git describe --exact-match HEAD >/dev/null 2>&1
